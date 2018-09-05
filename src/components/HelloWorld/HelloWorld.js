ddec07febafd54ae221aeb98f61558395b9c19bc
Miguel Ribeiro <miguel.ribeiro@blip.pt>
2018-09-05 19:16:01 +0200

    Added eslint and fixed linting issues

--- src/components/HelloWorld/HelloWorld.js
+++ src/components/HelloWorld/HelloWorld.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 
 const H1 = styled.h1`
   color: blue;
 `
 
-export default class HelloWorld extends Component {
-  render() {
-    const greeting = 'world';
+const HelloWorld = () => {
+  const greeting = 'world'
 
-    return (
-      <H1>Hello {greeting}!!</H1>
-    )
-  }
+  return (
+    <H1>Hello {greeting}!</H1>
+  )
 }
+
+export default HelloWorld

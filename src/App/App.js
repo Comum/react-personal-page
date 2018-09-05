66a5619bc0e6ec304fd3dcf5f12da77210da2d43
Miguel Ribeiro <miguel.ribeiro@blip.pt>
2018-09-05 19:20:43 +0200

    Change struture of folders

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import PropTypes from 'prop-types'
-import HelloWorld from '../HelloWorld/HelloWorld'
+import HelloWorld from '../components/HelloWorld/HelloWorld'
 
 const App = props => (
   <Provider store={props.store}>

41aebbbffe0c68b4259859f7cdc0701f95acaabd
Comum <miguel.rib.20@gmail.com>
2018-11-10 18:21:19 +0000

    Style easter egg page

--- src/scenes/EasterEgg.js
+++ src/scenes/EasterEgg.js
@@ -1,9 +1,35 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import Body from '../components/typography/Body'
+import fonts from '../theme/fonts'
+import colours from '../theme/colours'
+
+const TitleContainer = styled.div`
+    width: 100%;
+
+    padding-top: 50px;
+
+    font-size: ${fonts.sizes.small.mainTitle}
+    text-align: center;
+`
+
+const BodyContainer = styled.div`
+    margin-top: 20px;
+
+    text-align: center;
+`
+
 const EasterEgg = () => (
     <div>
-        I am the easter egg page
+        <TitleContainer>
+            I am the easter egg page
+        </TitleContainer>
+        <BodyContainer>
+            <Body colour="tertiary">
+                Nothing to see here though
+            </Body>
+        </BodyContainer>
     </div>
 )
 

a77d7f8b1ddde4f9b5b389693afb23cfa0a05b89
Comum <miguel.rib.20@gmail.com>
2018-12-02 00:54:12 +0000

    Handle works page data fetch

--- src/scenes/WorksPage.js
+++ src/scenes/WorksPage.js
@@ -1,12 +1,36 @@
 import React from 'react'
+import axios from 'axios'
 
 class WorksPage extends React.Component {
+    state = {
+        workId: this.props.match.params.id,
+        project: {}
+    }
+
+    componentDidMount() {
+        axios.get(`${window.location.origin}/assets/data/works.json`)
+        .then(response => {
+            if (response.data.length) {
+                response.data.forEach(project => {
+                    if (project.id === parseInt(this.state.workId, 10)) {
+                        this.setState({
+                            project: project
+                        })
+                    }
+                })
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
+
     render() {
-        const workId = this.props.match.params.id
+        console.log(this.state);
 
         return (
             <div>
-                Works page {workId}
+                Works page {this.state.workId}
             </div>
         )
     }

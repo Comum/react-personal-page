41aacd660eb2c5ba6a2e2cc8f83fcec85b23e0db
Comum <miguel.rib.20@gmail.com>
2018-12-11 23:05:04 +0000

    Continue with works header, add back button

--- src/scenes/components/WorksHeader/WorksHeader.js
+++ src/scenes/components/WorksHeader/WorksHeader.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
 import ColourContainer from '../../../components/ColourContainer/ColourContainer'
 import SectionTitle from '../../../components/SectionTitle/SectionTitle'
@@ -23,11 +24,23 @@ const TextWrapper = styled.div`
   margin: 30px 0;
 `
 
+const ImageContainer = styled.div`
+  position: absolute;
+  top: 16px;
+  right: 16px;
+
+  &:hover {
+      cursor: pointer;
+      border-bottom: 3px solid black;
+  }
+`
+
 const WorksHeader = ({ name, company }) => {
     const type = 'mainTitle'
     const projectColour = 'secondary'
     const companyColour = 'tertiary'
     const companyFont = 'body1'
+    const iconPath = '../assets/icons/home_icon.png'
 
     return (
         <ColourContainer>
@@ -45,6 +58,11 @@ const WorksHeader = ({ name, company }) => {
                         {company}
                     </Body>
                 </TextWrapper>
+                <Link to="/">
+                    <ImageContainer>
+                        <img src={iconPath} alt="" />
+                    </ImageContainer>
+                </Link>
             </SectionWrapper>
         </ColourContainer>
     )

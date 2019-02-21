71414f892e770b326700445fcbb5a0e174c53f8f
Comum <miguel.rib.20@gmail.com>
2019-02-21 23:31:27 +0000

    remove unneeded comments

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import App from './App/App'
-/*import Snow from './AuxScripts/snow'
+import App from './App/App';
 
-
-
-if (isItWinterTime()) {
-    // Let it snow
-    new Snow()
-}*/
-
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'));
